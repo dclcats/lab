01e2d7d0e6ec65cd5f8ebb0da8355ad3f970b3ee
Chris <chichucan@126.com>
2024-05-13 14:58:32 +0800

    Update index.tsx 添加网站 pv/uv

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -94,6 +94,11 @@ export default function Index(props: IRouteComponentProps) {
           <HeartFilled />
           &nbsp;&nbsp;<span>小冬笋</span>
         </div>
+        <div style={{textAlign: 'center'}}>
+          <span id="busuanzi_container_site_pv" style={{display: "inline"}}>本站总访问量 <span id="busuanzi_value_site_pv"></span> 次</span>
+          <span>|</span>
+          <span id="busuanzi_container_site_uv" style={{display: "inline"}}>本站总访客数 <span id="busuanzi_value_site_uv"></span> 人</span>
+        </div>
       </Footer>
     </div>
   );
